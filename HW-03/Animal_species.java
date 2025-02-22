f942f62f12bb79f324815177a3b40b115924be77
kananahmadov66 <111225262+kananahmadov66@users.noreply.github.com>
2025-02-22 16:40:33 +0400

    Add files via upload

---
+++ HW-03/Animal_species.java
@@ -0,0 +1,10 @@
+package happy_family_03;
+
+public enum animal_species {
+    DOG,
+    CAT,
+    FISH,
+    SPIDER,
+    BUNNY
+}
+
---
+++ HW-03/DAYSOFTHEWEEK.java
@@ -0,0 +1,12 @@
+package happy_family_03;
+
+public enum DAYSOFTHEWEEK {
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY,
+    SUNDAY
+}
+
