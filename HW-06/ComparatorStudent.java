f360cd9e69711e5ae9f8d7c4392e0752a46156b5
kananahmadov66 <111225262+kananahmadov66@users.noreply.github.com>
2025-03-15 23:02:58 +0400

    Update ComparatorStudent.java

--- HW-06/ComparatorStudent.java
+++ HW-06/ComparatorStudent.java
@@ -1,35 +1,12 @@
 import java.util.Comparator;
 
-public class ComparatorStudent implements Comparator<Student>
+public class ComparatorStudent
 {
-    @Override
-    public int compare(Student s1, Student s2)
-    {
-        if (s1.grade<s2.grade) return 1;
-        else if (s1.grade>s2.grade) return -1;
-        else
-        {
-            if (s1.birthYear<s2.birthYear) return -1;
-            else if (s1.birthYear > s2.birthYear) return 1;
-            else
-            {
-                if (s1.gender && !s2.gender) return 1;
-                else if (!s1.gender && s2.gender) return -1;
-                else
-                {
-                    if (s1.surname.compareTo(s2.surname) > 0) return 1;
-                    else if (s1.surname.compareTo(s2.surname) < 0) return -1;
-                    else
-                    {
-                        if (s1.name.compareTo(s2.name) > 0) return -1;
-                        else if (s1.name.compareTo(s2.name) < 0) return 1;
-                        else
-                        {
-                            return Integer.compare(s1.id, s2.id);
-                        }
-                    }
-                }
-            }
-        }
-    }
+    public static final Comparator<Student> STUDENT_COMPARATOR = Comparator
+            .comparingDouble((Student s) -> s.grade).reversed()
+            .thenComparingInt(s -> s.birthYear)
+            .thenComparing(s -> !s.gender)
+            .thenComparing(s -> s.surname)
+            .thenComparing(s -> s.name, Comparator.reverseOrder())
+            .thenComparingInt(s -> s.id);
 }
